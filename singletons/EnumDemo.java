ede15686d1823114d271c2af98cb315c876d7e1c
Kitty Yu <myownfish@163.com>
2020-04-20 11:59:21 +0800

    uncomment

--- singletons/EnumDemo.java
+++ singletons/EnumDemo.java
@@ -18,43 +18,45 @@ public class EnumDemo {
         System.out.println(sin.getValue());
         
 
-//        Singleton singletonSerizlize = Singleton.INSTANCE;
-//        singletonSerizlize.setValue(1);
-//
-//        FileOutputStream fileOut;
-//        try {
-//            fileOut = new FileOutputStream("out.ser");
-//            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-//            out.writeObject(singletonSerizlize);
-//            out.close();
-//            fileOut.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        singletonSerizlize.setValue(2);
-//
-//        Singleton singleton2 = null;
-//        try {
-//            FileInputStream fileIn = new FileInputStream("out.ser");
-//            ObjectInputStream in = new ObjectInputStream(fileIn);
-//            singleton2 = (Singleton) in.readObject();
-//            in.close();
-//            fileIn.close();
-//        } catch (IOException | ClassNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//        
-//        if (singletonSerizlize == singleton2) {
-//            System.out.println("Two objects are the same");
-//        } else {
-//            System.out.println("Two objects are not same");
-//        }
-//        
-//        System.out.println(singletonSerizlize.getValue());
-//        System.out.println(singleton2.getValue());
+        Singleton singletonSerizlize = Singleton.INSTANCE;
+        singletonSerizlize.setValue(1);
+
+        FileOutputStream fileOut;
+        try {
+            fileOut = new FileOutputStream("out.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(singletonSerizlize);
+            out.close();
+            fileOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        singletonSerizlize.setValue(2);
+
+        Singleton singleton2 = null;
+        try {
+            FileInputStream fileIn = new FileInputStream("out.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            singleton2 = (Singleton) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        
+        if (singletonSerizlize == singleton2) {
+            System.out.println("Two objects are the same");
+        } else {
+            System.out.println("Two objects are not same");
+        }
+        
+        System.out.println(singletonSerizlize.getValue());
+        System.out.println(singleton2.getValue());
     
         //reflection
+
         Singleton singleton = Singleton.INSTANCE;
+        Constructor<? extends Singleton>[] constructors = (Constructor<? extends Singleton>[]) singleton.getClass().getConstructors();
         Constructor<? extends Singleton> constructor = singleton.getClass().getDeclaredConstructor();
         constructor.setAccessible(true);
         
