825720d8ede08d095a961a87673edd9b90b1cb5e
myownfish <myownfish@163.com>
2020-05-25 17:14:21 +0800

    Create CyclicBarrierDemo.java

---
+++ cyclicbarrier/CyclicBarrierDemo.java
@@ -0,0 +1,83 @@
+package com.test.multithread.cyclicbarrier;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierDemo {
+
+    private CyclicBarrier cyclicBarrier;
+    private List<List<Integer>> partialResults = Collections.synchronizedList(new ArrayList<>());
+    private Random random = new Random();
+    private int NUM_PARTIAL_RESULTS;
+    private int NUM_WORKERS;
+
+    public static void main(String[] args) {
+        CyclicBarrierDemo demo = new CyclicBarrierDemo();
+        demo.runSimulation(5, 3);
+    }
+
+    public void runSimulation(int numWorkers, int numberOfPartialResults) {
+        NUM_PARTIAL_RESULTS = numberOfPartialResults;
+        NUM_WORKERS = numWorkers;
+
+        cyclicBarrier = new CyclicBarrier(numWorkers, new AggregatorThread());
+        System.out.println("Spawning " + NUM_WORKERS + " worker thread to compute " + NUM_PARTIAL_RESULTS + " partial results each");
+        for (int i = 0; i < NUM_WORKERS; i++) {
+            Thread worker = new Thread(new NumberCruncherThread());
+            worker.setName("Thread" + i);
+            worker.start();
+        }
+    }
+
+    class NumberCruncherThread implements Runnable {
+
+        @Override
+        public void run() {
+            String thisThreadName = Thread.currentThread().getName();
+            List<Integer> partialResult = new ArrayList<>();
+
+            for (int i = 0; i < NUM_PARTIAL_RESULTS; i++) {
+                Integer num = random.nextInt();
+                System.out.println(thisThreadName + ": Crunching some numbers! Final result " + num);
+                partialResult.add(num);
+            }
+            partialResults.add(partialResult);
+
+            try {
+                System.out.println(thisThreadName + " waiting for others to reach barrier.");
+                cyclicBarrier.await();
+            } catch (InterruptedException | BrokenBarrierException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    class AggregatorThread implements Runnable {
+
+        @Override
+        public void run() {
+            String thisThreadName = Thread.currentThread().getName();
+            System.out.println(
+                    thisThreadName + ": Computing sum of " + NUM_WORKERS
+                            + " workers, having " + NUM_PARTIAL_RESULTS + " results each.");
+            int sum = 0;
+
+            for (List<Integer> threadResult : partialResults) {
+                System.out.print(Thread.currentThread().getName() +" Adding ");
+                for (Integer partialResult : threadResult) {
+                    System.out.print(partialResult + " ");
+                    sum += partialResult;
+                }
+                System.out.println();
+            }
+            System.out.println(thisThreadName + ": Final result = " + sum);
+        }
+    }
+
+}
